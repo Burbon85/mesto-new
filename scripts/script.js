59884ce4abcf0cc96c87ffb16e078e0140326bce
Artur <dr.burbon85@yandex.ru>
2023-02-11 18:13:16 +0300

    mesto

---
+++ scripts/script.js
@@ -0,0 +1,38 @@
+const popupElement = document.querySelector('.popup');
+const popupOpenButtonElement = document.querySelector('.profile__edit_button');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close');
+let formElement = document.querySelector('.popup__submit');
+let nameInput = popupElement.querySelector('.popup__name');
+let jobInput = popupElement.querySelector('.popup__profile');
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+
+
+console.log (popupOpenButtonElement);
+
+
+const addPopupOpen = function() {
+    popupElement.classList.add('popup__opened');
+}
+
+const closePopupOpen = function() {
+    popupElement.classList.remove('popup__opened');
+    profileTitle.textContent = nameInput.value;
+    profileSubtitle.textContent = jobInput.value;
+}
+
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+    nameInput.value = nameProfile.textContent;
+    infoInput.value = infoProfile.textContent;
+}
+
+
+popupOpenButtonElement.addEventListener('click', addPopupOpen );
+
+popupCloseButtonElement.addEventListener('click', closePopupOpen );
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit);
+
+
