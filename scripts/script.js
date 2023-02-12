2a821296ff44d9f8e7d8295103a004abf181f60f
Artur <dr.burbon85@yandex.ru>
2023-02-12 17:24:25 +0300

    Исправление

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,29 @@
 const popupElement = document.querySelector('.popup');
 const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
-let formElement = document.querySelector('.popup__submit');
-let nameInput = popupElement.querySelector('.popup__name');
-let jobInput = popupElement.querySelector('.popup__profile');
+let formElement = document.querySelector('.popup__name');
+let nameInput = popupElement.querySelector('.popup__field_1');
+let jobInput = popupElement.querySelector('.popup__field_2');
 let profileTitle = document.querySelector('.profile__title');
 let profileSubtitle = document.querySelector('.profile__subtitle');
 
 
-console.log (popupOpenButtonElement);
-
 
 const addPopupOpen = function() {
-    popupElement.classList.add('popup__opened');
+    popupElement.classList.add('popup_opened');
+    nameInput.value = profileTitle.textContent;
+    jobInput.value = profileSubtitle.textContent;
 }
 
 const closePopupOpen = function() {
-    popupElement.classList.remove('popup__opened');
-    profileTitle.textContent = nameInput.value;
-    profileSubtitle.textContent = jobInput.value;
+    popupElement.classList.remove('popup_opened');
 }
 
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
-    nameInput.value = nameProfile.textContent;
-    infoInput.value = infoProfile.textContent;
+    nameProfile.textContent = nameInput.value;
+    infoProfile.textContent = infoInput.value;
+    closePopupOpen();
 }
 
 
